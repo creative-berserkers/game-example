ed6b13b692a0a2af4cf6e0690b419b22e01e18a9
Odrin White <odrinwhite@hotmail.com>
2015-07-08 23:30:44 +0200

    Designing data model

--- game/assets/skills/move.js
+++ game/assets/skills/move.js
@@ -1,28 +1,27 @@
-{
+export default {
   properties : {
     direction: "up"
-  }
-  description : "Move current player in {{direction}} direction"
+  },
+  description : "Move current player in {{direction}} direction",
   flags : [
     "can_be_triggered_by_event"
-  ]
+  ],
   cost : {
     movePoints : 1
-  }
-  handler : '''
-    return function(board, player){
-      if({{direction}} === "up"){
+  },
+  handler(board, player){
+      if(this.properties.direction === "up"){
         if(player.rank === 10){
           player.logger.warn("player_cant_move_above_10_rank")
         } else {
           player.rank = player.rank++
         }
-      } else if({{direction}} === "down"){
+      } else if(this.properties.direction === "down"){
         if(player.rank === 0){
           player.logger.warn("player_cant_move_below_0_rank")
         } else {
           player.rank = player.rank--
         }
       }
-  '''
+    }
 }
