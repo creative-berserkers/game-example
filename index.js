6bf00504d6b6cc69deedbf81490d5742d4aff343
Odrin White <odrinwhite@hotmail.com>
2015-07-11 22:38:37 +0200

    small refactoring

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-require('cb-hyperion').hyperion({
-    wss : require('cb-http-ws-server').Server(),
-    index : require('./lib/game-object').index
+import index from './game/src/game_object';
+import {Server} from 'cb-http-ws-server';
+import {hyperion} from 'cb-http-ws-server';
+
+hyperion({
+  wss : Server(),
+  index : index
 })
